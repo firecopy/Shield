56357c3b44afcc449289e8091c5827540931ccd0
ryanclancy000 <ryanclancy000@gmail.com>
2012-07-13 18:08:03 -0300

    Ryan likes clean(er) code

--- src/main/java/com/gmail/firecopy1/shield/Shield.java
+++ src/main/java/com/gmail/firecopy1/shield/Shield.java
@@ -12,25 +12,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Shield extends JavaPlugin {
 
-    public DamageListener dListener = new DamageListener(this);
-    public String pre = ChatColor.GRAY + "[" + ChatColor.GREEN + "Shield" + ChatColor.GRAY + "] ";
+    private final DamageListener dListener = new DamageListener(this);
     public List<String> disabledPlayers;
+    public int blockDamaged;
+    //
+    public static final ChatColor red = ChatColor.RED;
+    public static final ChatColor gray = ChatColor.GRAY;
+    public static final ChatColor green = ChatColor.GREEN;
+    public static final String pre = gray + "[" + green + "Shield" + gray + "] ";
 
     @Override
     public void onEnable() {
-        getConfig().options().copyDefaults(true);
-        disabledPlayers = getConfig().getStringList("disabledplayers");
-        saveConfig();
+        loadConfig();
         registerEvents();
         addRecipe();
-
         this.getCommand("shield").setExecutor(this);
     }
 
     @Override
     public void onDisable() {
-        getConfig().set("disabledplayers", disabledPlayers);
-        saveConfig();
+        disableConfig();
     }
 
     private void registerEvents() {
@@ -44,32 +45,37 @@ public class Shield extends JavaPlugin {
         Shield.setIngredient('B', Material.STRING);
         this.getServer().addRecipe(Shield);
     }
+    
+    private void loadConfig() {
+        this.getConfig().options().copyDefaults(true);
+        disabledPlayers = this.getConfig().getStringList("disabledplayers");
+        blockDamaged = this.getConfig().getInt("DamageBlocked", 4);
+        saveConfig();
+    }
+    
+    private void disableConfig() {
+        this.getConfig().set("disabledplayers", disabledPlayers);
+        saveConfig();
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        
         if (!(sender instanceof Player)) {
             sender.sendMessage("Command can only be run as a player. DOH!");
-            return false;
-        }
-        
-        if (args.length == 0) {
-            sender.sendMessage(pre + ChatColor.RED + "To few arguments!");
             return true;
         }
-        if (args.length > 1) {
-            sender.sendMessage(pre + ChatColor.RED + "To many arguments!");
-            return true;
-        }
-        if ("notice".equalsIgnoreCase(args[0])) {
+
+        if (args.length == 1 && "notice".equalsIgnoreCase(args[0])) {
             togglePlayers(sender);
-            return true;
+        } else {
+            sender.sendMessage(pre + red + "Invalid arguments!");
         }
 
         return true;
     }
 
     public void togglePlayers(CommandSender sender) {
-
         if (disabledPlayers.contains(sender.getName())) {
             disabledPlayers.remove(sender.getName());
             sender.sendMessage(pre + ChatColor.RED + "Shield Notice's enabled!");
@@ -77,6 +83,5 @@ public class Shield extends JavaPlugin {
             disabledPlayers.add(sender.getName());
             sender.sendMessage(pre + ChatColor.RED + "Shield Notice's disabled!");
         }
-
     }
 }
